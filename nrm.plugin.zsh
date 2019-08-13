13db59ed399322c8743c2e13a152a0ec8681eff3
Grigorii Horos <horosgrisa@gmail.com>
2019-08-13 12:20:08 +0300

    Initial commit

---
+++ nrm.plugin.zsh
@@ -0,0 +1,64 @@
+#!/usr/bin/env zsh
+
+function _nrm_args {
+  _arguments -s -A "-*"                                     \
+  '(-h --help)'{-h,--help}'[Show help information]'         \
+  '(-v --version)'{-v,--version}'[output the version number]'
+}
+
+function _nrm_help {
+  _arguments -s -A "-*"                           \
+  '(-h --help)'{-h,--help}'[Show help information]'
+}
+
+_nrm_repos=()
+nrm ls | sed 's/^..//' | sed -r '/^\s*$/d' | awk '{print $1" "$3}' | while read _repo; do
+  name="$(echo $_repo | awk '{print $1}' )"
+  description="$(echo $_repo | awk '{print $2}' )"
+  _nrm_repos+="${name}:${description}"
+done
+
+_nrm_commands=(
+  'ls:List all the registries'
+  'current:Show current registry name'
+  'use:Change registry to registry'
+  'add:Add one custom registry'
+  'set-auth:Set authorize information for a custom registry with a base64 encoded string or username and pasword'
+  'set-email:Set email for a custom registry'
+  'set-hosted-repo:Set hosted npm repository for a custom registry to publish packages'
+  'del:Delete one custom registry'
+  'home:Open the homepage of registry with optional browser'
+  'publish:Publish package to current registry if current registry is a custom registry'
+  'test:Show response time for specific or all registries'
+  'help:Print help'
+)
+
+_nrm(){
+  _nrm_args
+  _arguments                 \
+  '*:: :->subcmds' && return 0
+  
+  if (( CURRENT == 1 )); then
+    _describe -t commands 'Commands' _nrm_commands
+    return
+  fi
+  
+  if (( CURRENT == 2 )); then
+    
+    _nrm_help
+    case "$words[1]" in
+      use|del|home)
+        _describe -t commands "Nrm repos" _nrm_repos
+      ;;
+      puburl|ls|filestatus|sharelink)
+        _files
+      ;;
+      *)
+        return
+      ;;
+    esac
+    
+  fi
+}
+
+compdef _nrm nrm
